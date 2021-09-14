3a6ca1f3d12e76a127c6803519e93f93f61105d9
heebler <hans.keeb@gmail.com>
2021-09-14 20:24:03 +0300

    nft page with all details

--- src/components/nft/NFTItem.tsx
+++ src/components/nft/NFTItem.tsx
@@ -6,8 +6,16 @@ interface NFTItemProps {
     tokenId: string;
 }
 
+type Attribute = {
+    trait_type: string;
+    value: Number | string;
+    max_value: undefined | Number,
+    display_type: undefined | string;
+}
+
 export const NFTItem: FC<NFTItemProps> = props => {
     const [image, setImage] = useState("")
+    const [attributes, setAttributes] = useState<Attribute[]>([])
 
     useEffect(() => {
         if (!props?.tokenId) {
@@ -18,15 +26,30 @@ export const NFTItem: FC<NFTItemProps> = props => {
             .then(results => results.json())
             .then(data => {
                 setImage(data.image)
+                setAttributes(data.attributes as Attribute[])
             })
     });
 
+    const renderAttributes = () => {
+        return <div className="nft-attributes">
+            {attributes.map((attribute, index) => {
+                return <div key={index}>
+                    {attribute.trait_type}: <code>{attribute.value}{!!attribute.max_value ? `/${attribute.max_value}` : ""}</code>
+                </div>
+            })}
+        </div>
+    }
+
     return <div className="nft">
         <div>
-            <code>{props.tokenId} </code>
+            <code>Batch {props.tokenId.substring(65)} | Type {props.tokenId.substring(0, 8).toUpperCase()}</code>
         </div>
         <div>
-            <img src={image} alt="" width="200px" />
+            <img src={image} alt="" width="100%" />
         </div>
+        <div>
+            <p>Token state: <code className="word-breaker">{props.tokenId}</code></p>
+        </div>
+        {renderAttributes()}
     </div>
 }
